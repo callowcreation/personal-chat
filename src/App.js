883d5589df8f8712e815518c676c55c8a8fc3758
Jones Cropper <jones.cropper@quantiphi.com>
2025-06-07 16:34:57 -0700

    fix: 'token' is assigned a value but never used

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function SignIn() {
         auth.signInWithPopup(provider)
             .then((result) => {
                 // This gives you a Google Access Token. You can use it to access the Google API.
-                const token = result.credential.accessToken;
+                //const token = result.credential.accessToken;
                 // The signed-in user info.
                 const user = result.user;
                 console.log('User signed in:', user);
